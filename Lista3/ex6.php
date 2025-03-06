954d547904ae043187af73d22fabfae3198fd449
VictorMCordeiro <victorpositivos@gmail.com>
2025-03-05 21:24:59 -0300

    6
    tao simples, mas demorei,

--- Lista3/ex6.php
+++ Lista3/ex6.php
@@ -7,17 +7,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container mt-4 mb-3">
-    <h1 class=" mt-4 mb-3">Exercicio 5, lista 3</h1>
-    <h3>Faça um programa que leia o valor associado a um mês. Exemplo: 1 – 
-    Janeiro, 2 – Fevereiro... Exiba o nome do mês associado = USE SWITCH </h3>
+    <h1 class=" mt-4 mb-3">Exercicio 6, lista 3</h1>
+    <h3>Crie um formulário para que o usuário informe um número. Use um loop 
+    for para imprimir todos os números de 1 até o número informado. </h3>
 
-    <form method="post" class="mb-3" action="ex5res.php">   
+    <form method="post" class="mb-3" action="ex6res.php">   
         <div class="row">        
-            <div class="mb-3 mt-4 col-4">
-                <label for="n_mes" class="form-label">Insira uma valor correspondente a um mes: </label>
+            <div class="mb-3 mt-4 col-2">
+                <label for="n" class="form-label">Insira um valor: </label>
             </div>
             <div class="mb-3 mt-4 col-2">
-                <input type="number" id="n_mes" name="n_mes" class="form-control" required="" placeholder="(1 - 12)">
+                <input type="number" id="n" name="n" class="form-control" required="" placeholder="">
             </div>
             <div class="mb-3 mt-4 col-2">
                 <button type="submit" class="btn btn-primary">Enviar</button>  
@@ -29,4 +29,4 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
-</html>          
+</html>           
