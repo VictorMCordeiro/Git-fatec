25388c7da83dc8998bb8832cc80b5431fdc42542
VictorMCordeiro <victorpositivos@gmail.com>
2025-04-10 12:37:44 -0300

    .

--- Lista5/ex2.php
+++ Lista5/ex2.php
@@ -7,12 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container mt-4">
-    <h1>exercicio 1, Lista 5</h1>
-    <h3>Crie um formulário que leia dados de 5 contatos: nome e número de 
-telefone. Leia os dados e crie um mapa ordenado onde as chaves são os 
-nomes dos contatos e os valores são os números de telefone. Verifique se 
-há duplicatas de nome ou número de telefone antes de adicionar um novo 
-contato. Exiba a lista ordenada pelos nomes dos contatos. </h3>
+    <h1>exercicio 2, Lista 5</h1>
+    <h3>Crie um formulário que leia dados de 5 alunos: nome e três notas. Leia os 
+dados e crie um mapa ordenado onde as chaves são os nomes dos alunos 
+e os valores são as médias das notas. Exiba a lista de alunos ordenada pela 
+média das notas (do maior para o menor).</h3>
 
 
 <form method="post" action="ex1res.php">
--- Lista5/ex3.php
+++ Lista5/ex3.php
@@ -7,12 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container mt-4">
-    <h1>exercicio 1, Lista 5</h1>
-    <h3>Crie um formulário que leia dados de 5 contatos: nome e número de 
-telefone. Leia os dados e crie um mapa ordenado onde as chaves são os 
-nomes dos contatos e os valores são os números de telefone. Verifique se 
-há duplicatas de nome ou número de telefone antes de adicionar um novo 
-contato. Exiba a lista ordenada pelos nomes dos contatos. </h3>
+    <h1>exercicio 3, Lista 5</h1>
+    <h3>Crie um formulário que leia dados de 5 produtos, que são: código, nome e 
+preço. Leia os dados e crie um mapa ordenado onde as chaves são os 
+códigos dos produtos e os valores são também mapas ordenados com o 
+nome e o preço dos produtos. Aplique um desconto de 10% em todos os 
+produtos com preço acima de R$100,00 e exiba a lista ordenada pelo nome 
+do produto. </h3>
 
 
 <form method="post" action="ex1res.php">
